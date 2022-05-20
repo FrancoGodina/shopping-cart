cb2a55e4120769701b99581a7c8019a288d3ab38
franco <nahuelgodina@gmail.com>
2022-05-20 11:17:19 -0300

    Build main structure and data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import { BrowserRouter,  Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+import About from "./components/About";
 
 function App() {
     return(
@@ -10,6 +13,13 @@ function App() {
             <Route index element={
               <main className="index"><p>We are all about fashion.</p></main>
             }
+            />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<main style={{padding: "1rem"}}> 
+                                      <p>There's nothing to see here. </p>  
+                                    </main>} 
             />
           </Route>
         </Routes>
---
+++ src/components/data.js
@@ -0,0 +1,47 @@
+const data = {
+    dataItems:[
+        {
+            "id": 1,
+            "name": "Corduroy pants",
+            "img": "https://www.ramblersway.com/sites/default/files/styles/product_colorbox/public/product_photos/9755-Cocoa-01.jpg?itok=ygb5zdGD",
+            "price": 20.00,
+            "amount": 0
+        },
+
+        {
+            "id": 2,
+            "name": "Green hoodie",
+            "img": "https://m.media-amazon.com/images/I/71Zet+Y9ccL._AC_UL320_.jpg",
+            "price": 15.00
+        },
+
+        {
+            "id": 3,
+            "name": "Brown hoodie",
+            "img": "https://i.pinimg.com/originals/f5/98/67/f59867a5279ebb9ea1a7d866d0951f20.jpg",
+            "price": 15.99
+
+        },
+
+        {
+            "id": 4,
+            "name": "",
+            "img": ""
+        },
+
+        {
+            "id": 5,
+            "name": "Black hoodie",
+            "img": "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F6c%2F15%2F6c150d060d3d1aa95459daf18fd28d23f183c429.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
+            "price": 14.00
+        },
+
+        {
+            "id": 6,
+            "name": "",
+            "img": ""
+        }
+    ]
+}
+
+export default data;
