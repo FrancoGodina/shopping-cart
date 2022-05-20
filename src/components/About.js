3e1160a094f9b7f6aa1a2b4b2c78b85188f6cc3f
franco <nahuelgodina@gmail.com>
2022-05-20 11:49:35 -0300

    build about section

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+function About () {
+    return(
+        <div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nibh magna, scelerisque id blandit in, ultricies vitae est. Sed feugiat elementum laoreet. In suscipit blandit eros in dictum. Nunc convallis sit amet turpis non porttitor. Nulla luctus tristique erat nec convallis. Aliquam semper augue quis dapibus aliquet. Nullam sed urna sem. Suspendisse ullamcorper, nibh non tincidunt pharetra, nulla tellus gravida justo, a hendrerit nulla erat eget leo. Donec quis suscipit lectus, vitae convallis arcu. Morbi tempor, eros non faucibus aliquam, nisi nisl vulputate ligula, vel molestie tellus justo non mauris.</p>
+            </div>
+            <div>
+                <h4>Contact us: +54 12345678</h4>
+                <h4>Find us at: False St 432</h4>
+            </div>
+        </div>
+    )
+}
+
+export default About;
